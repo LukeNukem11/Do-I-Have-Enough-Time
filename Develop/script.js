2b2276072ca7ba03a28df7fc62aec55d4ab2b7d0
luke-nukem903 <lukeboren@gmail.com>
2023-05-27 00:15:39 -0500

    adjusted javascript file to clean up html

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,65 @@
-$(function () {
-  // Add a listener for click events on the save button
-  $(".saveBtn").on("click", function () {
-    // Get the user input from the textarea
-    var userInput = $(this).siblings(".description").val();
+$(function() {
+  var hours = ['9AM', '10AM', '11AM', '12PM', '1PM', '2PM', '3PM', '4PM', '5PM'];
 
-    // Get the id of the time-block containing the button that was clicked
-    var timeBlockId = $(this).closest(".time-block").attr("id");
+  var container = $('.container-fluid');
+  var currentHour = dayjs().format('H');
 
-    // Save the user input in local storage using the time block id as the key
-    localStorage.setItem(timeBlockId, userInput);
-  });
+  for (var i = 0; i < hours.length; i++) {
+    var hour = i + 9; // Calculate the hour value
+    var timeBlockDiv = $('<div>')
+      .attr('id', 'hour-' + hour)
+      .addClass('row time-block');
 
-  // Apply the past, present, or future class to each time block
-  $(".time-block").each(function () {
-    // Get the id of the time-block
-    var timeBlockId = $(this).attr("id");
+    var hourDiv = $('<div>')
+      .addClass('col-2 col-md-1 hour text-center py-3')
+      .text(hours[i]);
 
-    // Get the current hour using Day.js
-    var currentHour = dayjs().format("H");
+    var textarea = $('<textarea>')
+      .addClass('col-8 col-md-10 description')
+      .attr('rows', '3');
 
-    // Extract the hour from the timeBlockId (assuming it follows the format "hour-x")
-    var hour = parseInt(timeBlockId.split("-")[1]);
+    var saveButton = $('<button>')
+      .addClass('btn saveBtn col-2 col-md-1')
+      .attr('aria-label', 'save')
+      .append($('<i>').addClass('fas fa-save').attr('aria-hidden', 'true'));
 
-    // Check if the hour is within the range of 9 AM and 5 PM (inclusive)
-    if (hour >= 9 && hour <= 17) {
-      // Compare the hour to the current hour and add the appropriate class
-      if (hour < currentHour) {
-        $(this).addClass("past");
-      } else if (hour === currentHour) {
-        $(this).addClass("present");
-      } else {
-        $(this).addClass("future");
-      }
+    timeBlockDiv.append(hourDiv, textarea, saveButton);
+    container.append(timeBlockDiv);
+
+    if (hour < currentHour) {
+      timeBlockDiv.addClass('past');
+    } else if (hour == currentHour) {
+      timeBlockDiv.addClass('present');
     } else {
-      // Hide time blocks outside the desired range
-      $(this).hide();
+      timeBlockDiv.addClass('future');
     }
+  }
+
+  // Add a listener for click events on the save button
+  $('.saveBtn').on('click', function() {
+    var userInput = $(this).siblings('.description').val();
+    var timeBlockId = $(this).closest('.time-block').attr('id');
+    localStorage.setItem(timeBlockId, userInput);
+
+    // Display a message when item is saved
+    var saveMessage = $('<p>').text('Item saved to local storage').addClass('save-message');
+    $(this).siblings('.description').after(saveMessage);
+
+    // Remove the save message after a few seconds
+    setTimeout(function() {
+      saveMessage.remove();
+    }, 3000);
   });
 
   // Get user input from local storage and set the values of the corresponding textarea elements
-  $(".time-block").each(function () {
-    // Get the id of the time-block
-    var timeBlockId = $(this).attr("id");
-
-    // Get the user input from local storage using the time block id as the key
+  $('.time-block').each(function() {
+    var timeBlockId = $(this).attr('id');
     var userInput = localStorage.getItem(timeBlockId);
-
-    // Set the value of the textarea to the user input
-    $(this).find(".description").val(userInput);
+    $(this).find('.description').val(userInput);
   });
 
   // Display the current date in the header of the page
-  var currentDate = dayjs().format("dddd, MMMM D, YYYY");
-  $("#currentDay").text(currentDate);
+  var currentDate = dayjs().format('dddd, MMMM D, YYYY');
+  $('#currentDay').text(currentDate);
 });
+
